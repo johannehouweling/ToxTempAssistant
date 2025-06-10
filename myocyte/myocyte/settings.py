dd24eba24a33156907216dc597d386a169a55dd3
Jente Houweling <jente.houweling@rivm.nl>
2025-06-10 18:48:29 +0200

    fix overshadowing config

--- myocyte/myocyte/settings.py
+++ myocyte/myocyte/settings.py
@@ -109,7 +109,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "toxtempass.context_processors.config",
+                "toxtempass.context_processors.toxtempass_config",
             ],
         },
     },
