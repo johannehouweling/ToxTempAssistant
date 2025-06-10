f191026c32d5865a208228ae5a9b7cf47f4c36af
Jente Houweling <jente.houweling@rivm.nl>
2025-06-10 19:58:59 +0200

    bake git hash into container env GIT_HASH

--- myocyte/toxtempass/utilities.py
+++ myocyte/toxtempass/utilities.py
@@ -1,24 +1,7 @@
 import hashlib
 from pathlib import Path
 import subprocess
-from myocyte.settings import BASE_DIR
 
-def get_current_git_hash(short: bool = True) -> str:
-    
-    try:
-        # Get the full 40-char SHA
-        full_sha = (
-            subprocess
-            .check_output(['git', 'rev-parse', 'HEAD'], cwd=BASE_DIR, stderr=subprocess.DEVNULL)
-            .decode('ascii')
-            .strip()
-        )
-        # Shorten to 7 chars
-        short_sha = full_sha[:7]
-    except Exception:
-        short_sha = ''
-        full_sha = ''
-    return short_sha if short else full_sha
 
 def calculate_md5(pdf_file_path):
     """Calculate MD5 hash for a given PDF file."""
@@ -42,13 +25,13 @@ def calculate_md5_multiplefiles(files: list) -> dict:
     return md5dict
 
 
-def combine_dicts(dict1:dict, dict2:dict)->dict:
+def combine_dicts(dict1: dict, dict2: dict) -> dict:
     """Combine two dicts."""
     combined = {}
-    
+
     # Get all keys from both dictionaries
     keys = set(dict1.keys()).union(dict2.keys())
-    
+
     for key in keys:
         if key in dict1 and key in dict2:
             # If both values are dictionaries, recursively combine them
@@ -63,5 +46,5 @@ def combine_dicts(dict1:dict, dict2:dict)->dict:
         else:
             # If the key is only in the second dictionary
             combined[key] = dict2[key]
-    
+
     return combined
