78e6ea26e5c0dd699c24d2b38a6e418bd9be72d4
Matthias Arras <Matthias.Arras@avient.com>
2025-07-31 23:43:42 +0200

    fix(docker): typo

--- myocyte/django_startup.sh
+++ myocyte/django_startup.sh
@@ -53,7 +53,7 @@ sleep 0.1
 
 if [ "$TESTING" = "true" ]; then
   echo "Running tests because TESTING is true"
-  export DJANGO_SETTINGS_MODULE=toxtempass.mycote.settings
+  export DJANGO_SETTINGS_MODULE=myocyte.settings
   pytest -v --junitxml=/tmp/test-results/results.xml --cov=toxtempass --cov-report=xml:/tmp/test-results/coverage.xml
   exit $?
 fi
