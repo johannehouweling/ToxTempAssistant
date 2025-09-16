c40f687fa82a88006c4e63538f36e9f6526c3a2a
Matthias Arras <Matthias.Arras@avient.com>
2025-09-16 12:51:15 +0300

    feat(ui): add current URL context processor and update templates for improved navigation and onboarding

--- myocyte/toxtempass/context_processors.py
+++ myocyte/toxtempass/context_processors.py
@@ -1,4 +1,7 @@
+import logging
+
 from django.http import HttpRequest
+from django.urls import resolve
 
 from toxtempass import Config, config
 
@@ -6,3 +9,12 @@ from toxtempass import Config, config
 def toxtempass_config(request: HttpRequest) -> dict[str, Config]:
     """Include app config."""
     return {"config": config}
+
+
+def current_url_name(request: HttpRequest) -> dict[str, str | None]:
+    """Context processor to add the current URL name to the template context."""
+    try:
+        url_name = resolve(request.path_info).url_name
+    except Exception as exc:
+        logging.exception("Exception occurred while resolving URL name")
+    return {"current_url_name": url_name}
